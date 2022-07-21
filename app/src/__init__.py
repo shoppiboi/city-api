4310619d8e8e046dfb4a69bf3c96ff82a2fc85d2
Shoisob Ahasan <shoisob@vipra.tech>
2022-07-21 20:55:31 +0100

    refactor(app): set host of app based on environment variable

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -1,8 +1,10 @@
+import os
 import uvicorn
 from settings import load_config
 
 load_config()
 
+BACKEND_HOST = os.environ.get("BACKEND_HOST") 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host=BACKEND_HOST, port=8000, reload=True)
