da6649ebe65d54cff43e8a74953a549528812042
Shoisob Ahasan <shoisob@vipra.tech>
2022-07-21 18:21:55 +0100

    feat(app): load environment variables into main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 
+import os
+from settings import load_config
+
+load_config()
+
+CITY_API_HOST = os.environ.get("BACKEND_CITY_API_HOST")
+CITY_API_URL = os.environ.get("BACKEND_CITY_API_URL")
+CITY_API_KEY = os.environ.get("BACKEND_CITY_API_KEY")
+
+
 app = FastAPI()
 
-@app.get("/")
-def hello_world():
+@app.get("/availableCities")
+def get_cities():
+
+  query_parameters = {"minPopulation": "1000000"}
+
   return {"Hello": "World!"}
---
+++ app/settings.py
@@ -0,0 +1,22 @@
+import os
+import configparser
+
+
+def load_config():
+  """
+  Loads variables from ../environments/local.ini and
+  ../environments/local.secrets.ini and exports them as
+  environment variables to run app locally
+  """
+  config_file_paths = [
+      "../environments/local.ini",
+      "../environments/local.secrets.ini",
+  ]
+
+  for config_file_path in config_file_paths:
+      config = configparser.ConfigParser()
+      config.read(config_file_path)
+
+      for section in config.sections():
+          for key in config[section]:
+              os.environ[f"{section}_{key}".upper()] = config[section][key]
