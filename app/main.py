c2557e168552393ccc6102d1f5bea923ed10f000
Shoisob Ahasan <shoisob@vipra.tech>
2022-07-21 18:26:48 +0100

    feat(app): add nearbyCities endpoint

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ CITY_API_HOST = os.environ.get("BACKEND_CITY_API_HOST")
 CITY_API_URL = os.environ.get("BACKEND_CITY_API_URL")
 CITY_API_KEY = os.environ.get("BACKEND_CITY_API_KEY")
 
-
 app = FastAPI()
+
 
 @app.get("/availableCities")
 def get_cities():
@@ -36,3 +36,25 @@ def get_cities():
   cities = json.loads(response.text)["data"]
 
   return JSONResponse(content=cities)
+
+
+@app.get("/nearbyCities")
+def get_nearby_cities(city: int):
+
+  query_parameters = {"radius": "100"}  # radius wasn't specified in assignment brief, so just keeping it the default 100
+
+  headers = {
+    "X-RapidAPI-Key": CITY_API_KEY,
+    "X-RapidAPI-Host": CITY_API_HOST
+  }
+
+  response = requests.request(
+    "GET",
+    f"{CITY_API_URL}/{city}/nearbyCities", 
+    headers=headers,
+    params=query_parameters
+  )
+  
+  nearby_cities = json.loads(response.text)["data"]
+
+  return JSONResponse(content=nearby_cities)
