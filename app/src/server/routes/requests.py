12c60730c8dcef98c4391e447492b3ae346a5c94
Shoisob Ahasan <shoisob@vipra.tech>
2022-07-21 22:03:32 +0100

    chore(server): remove unnecessary post route

--- app/src/server/routes/requests.py
+++ app/src/server/routes/requests.py
@@ -2,18 +2,10 @@ from fastapi import APIRouter, Body
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
-from server.database import add_request, list_requests
+from server.database import list_requests
 from server.models import ResponseModel, RequestSchema
 
 router = APIRouter()
-
-
-@router.post("/", response_description="Request instance added into the database")
-async def add_request_data(request: RequestSchema = Body(...)):
-    request = jsonable_encoder(request)
-    new_request = await add_request(request)
-    return ResponseModel(new_request)
-
 
 @router.get("/listRequests")
 async def get_requests():
